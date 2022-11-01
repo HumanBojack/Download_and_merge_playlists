1c6ba4272e7feffbff84bc82d7392b78641c6c69
HumanBojack <spychalar59@gmail.com>
2022-11-01 19:12:41 +0100

    feat: add argparser to add cmd args

--- merge.py
+++ merge.py
@@ -1,21 +1,34 @@
+import os
 import ssl
 from tqdm import tqdm
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 from pytube.contrib.playlist import Playlist
+from argparse import ArgumentParser
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-playlist = Playlist("https://www.youtube.com/playlist?list=PLfiMjLyNWxeaQQjpPhk7FlbMZtD56RUyX")
-download_folder = "/Users/rom1/Documents/VSCode/merge_videos/tmp"
+def download_and_merge(playlist_url, download_folder, output_extension):
+    os.makedirs(download_folder, exist_ok=True)
+    playlist = Playlist(playlist_url)
+    video_files = []
 
-video_files = []
-for video in tqdm(playlist.videos):
-    stream = video.streams.filter(progressive=True).get_highest_resolution()
-    stream.download(download_folder)
-    file = f"{download_folder}/{stream.default_filename}"
+    i = 0
+    for video in tqdm(playlist.videos):
+        stream = video.streams.filter(progressive=True).get_highest_resolution()
+        stream.download(download_folder)
+        file = f"{download_folder}/{stream.default_filename}"
 
-    vfc = VideoFileClip(file)
-    video_files.append(vfc)
+        vfc = VideoFileClip(file)
+        video_files.append(vfc)
 
-output = concatenate_videoclips(video_files)
-output.write_videofile(f"{playlist.title}.mp4")
+    output = concatenate_videoclips(video_files)
+    output.write_videofile(f"{playlist.title}.{output_extension}")
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-U", "--playlist-url", help="Playlist url.", required=True)
+    parser.add_argument("-F", "--download-folder", help="Folder in which the playlist videos will be saved", default="./tmp")
+    parser.add_argument("-E", "--output-extension", help="Extension of the output file", default="mp4")
+
+    args = parser.parse_args()
+    download_and_merge(**vars(args))
